e793d1bae086198c817b6256bbf917efde1e8215
Steeve Pitis <steeve.pitis@gmail.com>
2020-11-24 17:27:04 +0100

    Updates

---
+++ precache-manifest.3f471b9581011adbcabb8a05d3670daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2ac75fb69865ade0580fb609ded3e7",
+    "url": "/3DHouse/index.html"
+  },
+  {
+    "revision": "fb64860eddab649f2c64",
+    "url": "/3DHouse/static/css/main.0c0fd41d.chunk.css"
+  },
+  {
+    "revision": "74b1a1294e49b39c9e8e",
+    "url": "/3DHouse/static/js/2.06098fec.chunk.js"
+  },
+  {
+    "revision": "1bc405a2f9ca5b8136d11cf29f9b2788",
+    "url": "/3DHouse/static/js/2.06098fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb64860eddab649f2c64",
+    "url": "/3DHouse/static/js/main.bb376737.chunk.js"
+  },
+  {
+    "revision": "766a117baac6a5e8422c",
+    "url": "/3DHouse/static/js/runtime-main.861e0737.js"
+  }
+]);
